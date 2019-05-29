a7ceed0dcba1d54ad8f6527cf273d0df7a3de4f5
vishal-pandey <contact@vishalpandey.xyz>
2019-05-29 21:56:16 +0530

    Build added

---
+++ precache-manifest.88beeccac02a04e180b1f35e639b28f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147b68f2395653122ec90c3ef078a3be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161a976999b480d6f935",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "161a976999b480d6f935",
+    "url": "/static/js/main.6249adeb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
